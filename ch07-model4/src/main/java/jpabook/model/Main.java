475ab7c2003349e0e5dd7e5084998fb4964c3faf
김태민 <seolin1218@gmail.com>
2022-07-26 13:45:33 +0300

    feature: 글로벌 페치 전략, 영속성 전이 설정, 엔티티 테스트

--- ch07-model4/src/main/java/jpabook/model/Main.java
+++ ch07-model4/src/main/java/jpabook/model/Main.java
@@ -1,5 +1,9 @@
 package jpabook.model;
 
+import jpabook.model.entity.Delivery;
+import jpabook.model.entity.Order;
+import jpabook.model.entity.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -22,6 +26,7 @@ public class Main {
 
             tx.begin(); //트랜잭션 시작
             //TODO 비즈니스 로직
+            makeOrder(em);
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -34,4 +39,16 @@ public class Main {
         emf.close(); //엔티티 매니저 팩토리 종료
     }
 
+    public static void makeOrder(EntityManager em) {
+        Delivery delivery = new Delivery();
+        OrderItem orderItem1 = new OrderItem();
+        OrderItem orderItem2 = new OrderItem();
+
+        Order order = new Order();
+        order.setDelivery(delivery);
+        order.addOrderItem(orderItem1);
+        order.addOrderItem(orderItem2);
+
+        em.persist(order);
+    }
 }
